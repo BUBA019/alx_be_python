485b0f716c479da4bfcdf0fc2516fc2817668618
Buba Yakubu Ahmad <ahmadbubayakubu@gmail.com>
2025-07-10 11:53:33 +0100

    basic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+sub = number1 - number2
+prod = number1 * number2
+print("Addition of",number1,"and",number2,"is",sum)
+print("Subtration of",number1,"and",number2,"is",sub)
+print("Multiplication of",number1,"and",number2,"is",prod)
